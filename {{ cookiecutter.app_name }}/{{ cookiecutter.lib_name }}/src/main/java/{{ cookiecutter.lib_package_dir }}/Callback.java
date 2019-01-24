41131980f63c448b9ded64736cfa33b987f5d91e
RomellFudi <romllz489@gmail.com>
2019-01-24 12:07:17 -0500

    delete unnecessay files

--- {{ cookiecutter.app_name }}/{{ cookiecutter.lib_name }}/src/main/java/{{ cookiecutter.lib_package_dir }}/Callback.java
+++
@@ -1,8 +0,0 @@
-package {{ cookiecutter.lib_package_name }};
-
-public abstract class Callback<T extends DataResponse> {
-
-    public abstract void throwError(APIException exception);
-
-    public abstract void response(T bean);
-}
--- {{ cookiecutter.app_name }}/{{ cookiecutter.lib_name }}/src/main/java/{{ cookiecutter.lib_package_dir }}/Module.java
+++
@@ -1,5 +0,0 @@
-package {{ cookiecutter.lib_package_name }};
-
-public interface Module {
-    void sendData(int hash, DataResponse dataResponse );
-}
